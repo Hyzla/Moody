c8021fdd56fbbb0241a12463172249085375d174
Raphaël Martel <contact@raphaelmartel.com>
2023-04-02 13:54:42 +0200

    Convert form data in object

--- scripts/register.js
+++ scripts/register.js
@@ -4,6 +4,9 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  console.log(data);
 });
 
 const togglePassword = () => {
